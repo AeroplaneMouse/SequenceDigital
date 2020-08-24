a675eb89efd34115272b81a498b47222bb7ec351
Daniel Fly <danielfly42@gmail.com>
2020-08-25 01:30:33 +0200

    Added card and player classes

---
+++ Models/Card.cs
@@ -0,0 +1,21 @@
+﻿namespace SD.Models
+{
+    enum Suit { Diamonds, Clubs, Hearts, Spades}
+    enum Cvalue { One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace}
+
+    class Card
+    {
+        public Suit Suit { get; private set; }
+        public Cvalue Value { get; private set; }
+        public int ColorGroup { get; private set; }
+
+        public Card(Suit suit, Cvalue value)
+        {
+            Suit = suit;
+            Value = value;
+
+            // Set color group. 0 for red, 1 for black.
+            ColorGroup = (suit == Suit.Diamonds || suit == Suit.Hearts) ? 0 : 1;
+        }
+    }
+}
---
+++ Models/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SD.Models
+{
+    class Player
+    {
+        private static int _id = 0;
+
+        public int ID { get; }
+        public string Name { get; set; }
+
+        public Player(string name)
+        {
+            ID = _id++;
+            Name = name;
+        }
+
+        public void PlayCard()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReturnCards()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
